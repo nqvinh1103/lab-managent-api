bc046457c90fe2fac92530273dac7fb1ed4c9518
nqvinh1103 <162325959+nqvinh1103@users.noreply.github.com>
2025-10-29 14:21:33 +0700

    Refactor route imports in index.ts for better organization

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express'
 import authRoutes from './modules/auth.route'
-import roleRoutes from './modules/role.route'
-import userRoutes from './modules/user.route'
-import privilegeRoutes from './modules/privilege.route'
-import testOrderRoutes from './modules/testOrder.route'
+import instrumentReagentRoutes from './modules/instrumentReagent.route'
 import patientRoutes from './modules/patient.route'
-import instrumentReagentRoutes from './modules/instrumentReagent.routes';
-
+import privilegeRoutes from './modules/privilege.route'
+import roleRoutes from './modules/role.route'
+import testOrderRoutes from './modules/testOrder.route'
+import userRoutes from './modules/user.route'
 
 const router = Router()
 
@@ -28,5 +27,5 @@ router.use('/test-orders', testOrderRoutes)
 router.use('/patients', patientRoutes)
 
 // Instrument Reagent routes
-router.use('/instrument-reagents', instrumentReagentRoutes);
+router.use('/instrument-reagents', instrumentReagentRoutes)
 export default router
